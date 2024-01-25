73bb3921168f4a4aeef1029c2ce00d0cc585c679
SaaraUthmaan <149133492+SaaraUthmaan@users.noreply.github.com>
2024-01-25 12:13:13 -0800

    Update and rename RandomTry to LuckTester.java

--- LuckTester.java
+++ LuckTester.java
@@ -1,18 +1,14 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class RandomTry {
+public class LuckTester {
     public static void main(String[] args) {
         Scanner userReply = new Scanner(System.in);
-        System.out.println("Pick a number between 1 - 10");
+        System.out.println("Pick a number");
         int userNumber = userReply.nextInt();
         
-        while(userNumber > 10){
-            System.out.println("Please Pick a number between 1 - 10");
-             userNumber = userReply.nextInt(); 
-        }
         Random luck = new Random();
-            int number = luck.nextInt(10) + 1;
+            int number = luck.nextInt(Integer.MAX_VALUE) + 1;
             System.out.println(number + " ");
            
            int difference = userNumber - number; 
@@ -20,7 +16,7 @@ public class RandomTry {
         if(number == userNumber){
             System.out.println("You are an AMAZING Guesser!");
         }else{
-            System.out.println("You were off by "+ Math.abs(difference) +" points. Try arder next time.");
+            System.out.println("You were off by "+ Math.abs(difference) +" . Try harder next time.");
         }    
     }
     
